4fe6e5a9cd4882710de87613867a088aeb3e3390
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-24 15:53:39 -0400

    Create conditional for sprites, show front and back

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,24 @@
-import React from "react"
+import React, { useState } from "react"
 import { Card } from "semantic-ui-react"
 
 function PokemonCard({ pokemon }) {
   const { id, name, hp, sprites } = pokemon
   const { front, back } = sprites
+  const [spriteState, setSpriteState] = useState(false)
+
+  function handleOnClick() {
+    setSpriteState((spriteState) => !spriteState)
+  }
+
   return (
     <Card>
       <div>
         <div className="image">
-          <img alt="oh no!" src={front} />
+          {spriteState === false ? (
+            <img alt={name} src={front} onClick={handleOnClick} />
+          ) : (
+            <img alt={name} src={back} onClick={handleOnClick} />
+          )}
         </div>
         <div className="content">
           <div className="header">{name}</div>
