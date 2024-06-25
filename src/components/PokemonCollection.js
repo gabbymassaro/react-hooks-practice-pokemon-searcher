d44de70f79afd960a6094e289083bb4c9945c5b6
Gabriella Massaro <gmassaro807@gmail.com>
2024-06-24 20:34:20 -0400

    Search by pokemon name works

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,12 +2,13 @@ import React from "react"
 import PokemonCard from "./PokemonCard"
 import { Card } from "semantic-ui-react"
 
-function PokemonCollection({ pokemonList }) {
+function PokemonCollection({ pokemonList, onSearchForPokemon }) {
+  const renderSearchedPokemon =
+    onSearchForPokemon.length > 0 ? onSearchForPokemon : pokemonList
   return (
     <div>
-      <h1>Hello From Pokemon Collection</h1>
       <Card.Group itemsPerRow={6}>
-        {pokemonList.map((pokemon) => (
+        {renderSearchedPokemon.map((pokemon) => (
           <PokemonCard key={pokemon.id} pokemon={pokemon} />
         ))}
       </Card.Group>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -6,6 +6,7 @@ import { Container } from "semantic-ui-react"
 
 function PokemonPage() {
   const [pokemonList, setPokemonList] = useState([])
+  const [search, setSearch] = useState("")
 
   useEffect(() => {
     fetch("http://localhost:3001/pokemon")
@@ -13,15 +14,28 @@ function PokemonPage() {
       .then((data) => setPokemonList(data))
   }, [])
 
+  function handleSearchInput(input) {
+    setSearch(input)
+  }
+
+  const onSearchForPokemon = pokemonList.filter((pokemon) => {
+    if (search === "") return true
+
+    return pokemon.name.toLowerCase().includes(search.toLowerCase())
+  })
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search search={search} onSearch={handleSearchInput} />
       <br />
-      <PokemonCollection pokemonList={pokemonList} />
+      <PokemonCollection
+        pokemonList={pokemonList}
+        onSearchForPokemon={onSearchForPokemon}
+      />
     </Container>
   )
 }
